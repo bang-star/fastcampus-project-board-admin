cc2dca9733437bc94eaf0e1dfea57e54c955c593
bang-star <63120360+bang-star@users.noreply.github.com>
2023-03-29 00:12:28 +0900

    데이터베이스 접근 로직 테스트 정의 (#18)
    * #8 - 회원정보에 접근할 Repository 인터페이스, JPA 설정 및 JPA 테스트 작성
    
    회원 데이터에 접근할 Repository 인터페이스와
    jpa auditing 기능을 활성화시킬 `JpaConfig`,
    각자 다른 권한을 가진 테스트용 회원 데이터,
    crud 쿼리 생성을 확인할 jpa 테스트 작성
    
    ---------
    
    Co-authored-by: bang-star <bangseongmin@naver.com>

---
+++ src/main/java/com/example/projectboardadmin/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.projectboardadmin.repository;
+
+import com.example.projectboardadmin.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
